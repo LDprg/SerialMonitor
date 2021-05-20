7fa93b7eef1c7ee9c50e182dd316b9b20ca0b2c1
LDprg <71488985+LDprg@users.noreply.github.com>
2021-05-20 18:41:04 +0200

    format code

--- SerialMonitor/src/main.cpp
+++ SerialMonitor/src/main.cpp
@@ -9,180 +9,177 @@
 #include <vector>
 #include <limits>
 
-#define IM_ARRAYSIZE(_ARR)          ((int)(sizeof(_ARR) / sizeof(*(_ARR)))) 
+#define IM_ARRAYSIZE(_ARR)          ((int)(sizeof(_ARR) / sizeof(*(_ARR))))
 
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 GLFWwindow* window;
 
 std::pair<std::vector<float>, std::vector<float>> data = {
-    {1,2,3,4,5},
-    {1,2,4,8,16}
+	{1,2,3,4,5},
+	{1,2,4,8,16}
 };
 
 void draw()
 {
-    ImGui::Begin("Config");
-    ImGui::Button("Hello!");
-    ImGui::End();
+	ImGui::Begin("Config");
+	ImGui::Button("Hello!");
+	ImGui::End();
 
-    ImGui::Begin("Graph", 0, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
-    
+	ImGui::Begin("Graph", 0, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 
-    if (ImPlot::BeginPlot("Graph", "x", "y", ImGui::GetContentRegionAvail(), ImPlotFlags_AntiAliased))
-    {
+	if (ImPlot::BeginPlot("Graph", "x", "y", ImGui::GetContentRegionAvail(), ImPlotFlags_AntiAliased))
+	{
+		ImPlot::PlotLine("Data", &data.first[0], &data.second[0], data.first.size());
 
-        ImPlot::PlotLine("Data", &data.first[0], &data.second[0], data.first.size());
-        
-        ImPlot::EndPlot();
-    }
+		ImPlot::EndPlot();
+	}
 
-    //ImGui::PlotLines("", &data[0], data.size(), 0, "Data", std::numeric_limits<float>::min(), std::numeric_limits<float>::max(), ImGui::GetContentRegionAvail());
+	//ImGui::PlotLines("", &data[0], data.size(), 0, "Data", std::numeric_limits<float>::min(), std::numeric_limits<float>::max(), ImGui::GetContentRegionAvail());
 
-        
-    ImGui::End();
+	ImGui::End();
 
-    ImGui::ShowDemoWindow();
+	ImGui::ShowDemoWindow();
 }
 
 void drawMenue()
 {
-    if (ImGui::BeginMenuBar())
-    {
-        if (ImGui::BeginMenu("File"))
-        {
-            ImGui::Separator();
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::Separator();
 
-            glfwSetWindowShouldClose(window, ImGui::MenuItem("Exit"));
-            
-            ImGui::EndMenu();
-        }
+			glfwSetWindowShouldClose(window, ImGui::MenuItem("Exit"));
 
-        ImGui::EndMenuBar();
-    }
+			ImGui::EndMenu();
+		}
+
+		ImGui::EndMenuBar();
+	}
 }
 
 void drawWorkspace()
 {
-    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 
-    ImGuiViewport* viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(viewport->GetWorkPos());
-    ImGui::SetNextWindowSize(viewport->GetWorkSize());
-    ImGui::SetNextWindowViewport(viewport->ID);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+	ImGuiViewport* viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(viewport->GetWorkPos());
+	ImGui::SetNextWindowSize(viewport->GetWorkSize());
+	ImGui::SetNextWindowViewport(viewport->ID);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
-    ImGui::Begin("Workspace", 0, window_flags);
+	ImGui::Begin("Workspace", 0, window_flags);
 
-    ImGui::PopStyleVar(2);
+	ImGui::PopStyleVar(2);
 
-    ImGui::TextUnformatted("Workspace:");
+	ImGui::TextUnformatted("Workspace:");
 
-    // DockSpace
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-    {
-        ImGuiID dockspace_id = ImGui::GetID("Workspace");
-        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-    }
+	// DockSpace
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+	{
+		ImGuiID dockspace_id = ImGui::GetID("Workspace");
+		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+	}
 
-    drawMenue();
+	drawMenue();
 
-    ImGui::End();
+	ImGui::End();
 }
 
 int main()
 {
-    glfwInit();
+	glfwInit();
 
-    window = glfwCreateWindow(1024, 768, "TEST", NULL, NULL);
+	window = glfwCreateWindow(1024, 768, "SerialMonitor", NULL, NULL);
 
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize OpenGL context" << std::endl;
-        return -1;
-    }
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImPlot::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize OpenGL context" << std::endl;
+		return -1;
+	}
 
-    // Setup Platform/Renderer bindings
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImPlot::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 450");
 
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.WindowMenuButtonPosition = ImGuiDir_None;
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
+	// Setup Dear ImGui style
+	ImGui::StyleColorsClassic();
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.WindowMenuButtonPosition = ImGuiDir_None;
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
 
-        // feed inputs to dear imgui, start new frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
 
-        drawWorkspace();
-        
-        draw();
-        
-        // Render dear imgui into screen
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-        {
-            GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
+		drawWorkspace();
 
-        glfwSwapBuffers(window);
-    }
+		draw();
 
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    ImPlot::DestroyContext();
+		// Render dear imgui into screen
+		// Rendering
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	ImPlot::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
 
 #ifdef RELEASE
 
 int WinMain()
 {
-    return main();
+	return main();
 }
 
 #endif
